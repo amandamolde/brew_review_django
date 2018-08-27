513c95025f9d0a73e31985478c78e02c7d2b509c
amandamolde <amanda.molde@gmail.com>
2018-08-27 15:14:37 -0600

    Commiting .gitignore

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/amandamolde/ga_wdi_funky_ducks/brew_review_django/.env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
