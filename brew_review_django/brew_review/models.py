c00c96a197e815a3776194d63c2ea3d3ef16b6c5
amandamolde <amanda.molde@gmail.com>
2018-08-27 15:23:59 -0600

    Added models

--- brew_review_django/brew_review/models.py
+++ brew_review_django/brew_review/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Brewery(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    description = models.TextField()
+    website_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE, related_name='reviews')
+    atmosphere = models.PositiveIntegerField()
+    beer_tenders = models.PositiveIntegerField()
+    beer_selection = models.PositiveIntegerField()
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.notes
