e72a24141a8e4006f3e76fc599b23039ae1e6b5f
amandamolde <amanda.molde@gmail.com>
2018-08-28 10:51:48 -0600

    Added id to serializers

--- brew_review_django/brew_review/serializers.py
+++ brew_review_django/brew_review/serializers.py
@@ -4,9 +4,9 @@ from .models import Brewery, Review
 class BrewerySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Brewery
-        fields = ('name', 'city', 'state', 'description', 'website_url',)
+        fields = ('id', 'name', 'city', 'state', 'description', 'website_url',)
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Review
-        fields = ('brewery', 'atmosphere', 'beer_tenders', 'beer_selection', 'notes', 'photo',)
+        fields = ('id', 'brewery', 'atmosphere', 'beer_tenders', 'beer_selection', 'notes', 'photo',)
